aecaeefebc6038ff7dd35c5a6569ad9b0aaab8ea
amaralc <amaral.calil@gmail.com>
2020-10-04 18:40:36 -0300

    TEST / Remove useless variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
 
-const categories = [
-  { id: 'labs', title: "Kingdom: Animal", subcategories: ['1', '2', '3', '4'] },
-  { id: '1', title: "Phylum: Chordata", subcategories: ['2'] },
-  { id: '2', title: "Clade: Synapsida", subcategories: ['3'] },
-  { id: '3', title: "Class: Mammailia", subcategories: ['4'] },
-  { id: '4', title: "Order: Carnivora", subcategories: ['5'] },
-  { id: '5', title: "Subfamily: Felinae", subcategories: ['6'] },
-  { id: '6', title: "Genus: Felis", subcategories: ['7'] },
-  { id: '7', title: "Species: Felis catus", subcategories: [] }
-];
 
 
 const Lab = ({match}) => {
@@ -26,7 +16,7 @@ const Category = ({ match }) => {
   //   return match.params.catId === category.id;
   // });
 
-  const lab = match.url;
+
   return (
     <>
       {match.isExact && (
